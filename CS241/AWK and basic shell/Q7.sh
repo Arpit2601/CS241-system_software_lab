074825877f2808b533c0e65001da5f7284669903
arpit-iitg <arpit170101012@iitg.ac.in>
2018-10-23 00:00:12 +0530

    recursive file search done

--- CS241/AWK and basic shell/Q7.sh
+++ CS241/AWK and basic shell/Q7.sh
@@ -15,5 +15,18 @@ echo $len
 #part3
 cd $1
 rm *.c
+#part 1 with recursive searches
+echo enter the file to be searched
+read file
+list=$(find $1 -name $file)
+#echo $list
+num=${#list}
+#echo $num
+if (( num != 0 ))
+then
+        echo file exists
+else
+        echo file does not exist
+fi
 #part 4
-find $1 -type f -size 0b -delete
+find $1 -type f -size 0b -delete    #does recursively
