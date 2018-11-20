137e98dd286394ef449fcf94c9c0c9bb5201acd0
arpit-iitg <arpit170101012@iitg.ac.in>
2018-11-20 11:53:49 +0530

    lab done

--- CS241/AWK and basic shell/Q8.sh
+++ CS241/AWK and basic shell/Q8.sh
@@ -1,2 +1,11 @@
 #! /bin/bash
 cp $1 newfile
+# directory=$(echo $(basename $1))
+# file=$1
+# len1=${#directory}
+# len2=${#file}
+# final_directory=${file:0:$len2-$len1}
+# echo $final_directory
+# # directory=$(echo $1 | grep "*/[a-z]+")d
+# cd $final_directory
+# cp $1 newfile
